da4be9ea619d7a7e984e849a62b11ccaf7f416d1
SouravKhiching <148612800+SouravKhiching@users.noreply.github.com>
2023-11-05 20:22:14 +0530

    Add files via upload

---
+++ Quiz App/Readme.txt
@@ -0,0 +1,12 @@
+This folder contains all the files related to my Web Development Internship project, "Quiz App".
+
+The files included are:
+	i) index.html - Contains the script of the index or Home page.
+	ii) style.css - Contains the CSS script which manages the designs used in index.html.
+	iii) script.js - Contains the JavaScript code which adds the functions to different elements for the page index.html.
+	iv) Background.jpg - The background photo of the webpage.
+	v) instructions.html, instructions.css - Contains the script and design of the Instructions page.
+	vi) about.html, about.css - Contains the script and design of the About page.
+	vii) questions.js - Contains the questions used in the web application.
+
+Copy all the related files and run the code to see the working of the webpage.
---
+++ Quiz App/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does CPU stand for?",
+        answer: "B. Central Processing Unit",
+        options: [
+            "A. Central Programming Unit",
+            "B. Central Processing Unit",
+            "C. Computer Personalization Unit",
+            "D. Central Print Utility"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which programming language is known for its use in web development and is often used for both client-side and server-side scripting?",
+        answer: "C. JavaScript",
+        options: [
+            "A. Python",
+            "B. Java",
+            "C. JavaScript",
+            "D. C++"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following is not an operating system?",
+        answer: "D. Microsoft Office",
+        options: [
+            "A. Windows",
+            "B. MacOS",
+            "C. Linux",
+            "D. Microsoft Office"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which protocol is used for sending and receiving emails?",
+        answer: "C. SMTP",
+        options: [
+            "A. HTTP",
+            "B. FTP",
+            "C. SMTP",
+            "D. TCP"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which company developed the first commercially successful computer mouse?",
+        answer: "D. Xerox",
+        options: [
+            "A. IBM",
+            "B. Microsoft",
+            "C. Apple",
+            "D. Xerox"
+        ]
+    },
+];
